97fc9da9b4e93790844deecc11043925aa4cae80
Cat Kretzschmar <cat.ktz@gmail.com>
2019-12-10 13:12:58 -0500

    basic website built

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//    //Current Date
+//    let m = moment().format("dddd" + " Do" + " MMMM");
+//    var x = document.getElementById("date").textContent = (m)
+//    console.log(`toString() => ${m.toString()}`);
+
+// var nineAm = document.querySelector("#NineAm");
+// var nineAmSaveButton = document.querySelector("#NineamSave");
+
+// nineAmSaveButton.addEventListener("submit", save); 
+
+// function save() {
+// console.log ("got here");
+// localStorage.setItem("9am", JSON.stringify(nineAm.value.trim()));
+// }
+
+// // Retrieve the object from storage
+// var retrievedObject = localStorage.getItem('testObject');
+
+// console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+    // you can use jquery for this!!
+
+    // default is green --- in the future
+    // current hour is red 
+    // past hour is wheat
