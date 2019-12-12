5dcc1065e3ca056cbf6602a935f87ff490667a00
Cat Kretzschmar <cat.ktz@gmail.com>
2019-12-12 16:44:33 -0500

    time colors work

--- script.js
+++ script.js
@@ -1,18 +1,77 @@
+ //Current Date
+ let m = moment().format("dddd" + " Do" + " MMMM");
+ var x = document.getElementById("date").textContent = (m)
+ console.log(`${m.toString()}`);
 
-//    //Current Date
-//    let m = moment().format("dddd" + " Do" + " MMMM");
-//    var x = document.getElementById("date").textContent = (m)
-//    console.log(`toString() => ${m.toString()}`);
+ // take current time and apply to rows if in past wheat current red future green
 
-// var nineAm = document.querySelector("#NineAm");
-// var nineAmSaveButton = document.querySelector("#NineamSave");
+ let t = moment().format("hh" + ":mm" + ":ss");
+ console.log(`${t.toString()}`);
 
-// nineAmSaveButton.addEventListener("submit", save); 
+ var twelvePm = document.querySelector("#TwelvePm");
+let time = moment().format("hh");
+time = `${time.toString()}`;
+console.log(`${time.toString()}`);
+if (time === 09) {
+    console.log("got here");
+    $("#NineAm").addClass("present");
+    $("#TenAm, #ElevenAm, #TwelvePm, #OnePm, #TwoPm, #ThreePm, #FourPm").addClass("future");
+}
+else if (time === 10) {
+    console.log("got here");
+ $("#TenAm").addClass("present");
+ $("#NineAm").addClass("past");
+ $("#ElevenAm, #TwelvePm, #OnePm, #TwoPm, #ThreePm, #FourPm").addClass("future");
+}
+else if (time === 11) {
+    console.log("got here");
+ $("#ElevenAm").addClass("present");
+ $("#NineAm, #TenAm").addClass("past");
+ $("#TwelvePm, #OnePm, #TwoPm, #ThreePm, #FourPm").addClass("future");
+}
+else if (time === 12) {
+    console.log("got here");
+ $("#TwelvePm").addClass("present");
+ $("#NineAm, #TenAm, #ElevenAm").addClass("past");
+ $("#OnePm, #TwoPm, #ThreePm, #FourPm").addClass("future");
+}
+else if (time === 01) {
+    console.log("got here");
+ $("#OnePm").addClass("present");
+ $("#NineAm, #TenAm, #ElevenAm, #TwelvePm").addClass("past");
+ $("#TwoPm, #ThreePm, #FourPm").addClass("future");
+}
+else if (time === 02) {
+    console.log("got here");
+ $("#TwoPm").addClass("present");
+ $("#NineAm, #TenAm, #ElevenAm, #TwelvePm, #OnePm,").addClass("past");
+ $("#ThreePm, #FourPm").addClass("future");
+}
+else if (time === 03) {
+    console.log("got here");
+ $("#ThreePm").addClass("present");
+ $("#NineAm, #TenAm, #ElevenAm, #TwelvePm, #OnePm, #TwoPm").addClass("past");
+ $("#FourPm").addClass("future");
+}
+else if (time === "04") {
+    console.log("got here");
+ $("#FourPm").addClass("present");
+ $("#NineAm, #TenAm, #ElevenAm, #TwelvePm, #OnePm, #TwoPm, #ThreePm").addClass("past");
+}
 
-// function save() {
-// console.log ("got here");
-// localStorage.setItem("9am", JSON.stringify(nineAm.value.trim()));
-// }
+
+let DaysSchedule = [];
+
+$(".saveButton").on("click", function(event) {
+    event.preventDefault();  
+    let key = $(this).parents("tr").children(".userInput").attr("id") 
+    let value = $(this).parents("tr").children(".userInput").children(".middle").val(); 
+    let userData = {key: value}
+    console.log (userData);  
+    DaysSchedule.push(userData); 
+    localStorage.setItem(key, value);
+});
+
 
 // // Retrieve the object from storage
 // var retrievedObject = localStorage.getItem('testObject');
